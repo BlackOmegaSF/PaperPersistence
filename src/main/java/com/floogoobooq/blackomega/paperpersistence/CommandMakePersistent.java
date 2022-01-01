90f8b9d847d02fe2358df9a0f55232e426696acd
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-01 11:57:43 -0800

    Initial commit

---
+++ src/main/java/com/floogoobooq/blackomega/paperpersistence/CommandMakePersistent.java
@@ -0,0 +1,109 @@
+package com.floogoobooq.blackomega.paperpersistence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class CommandMakePersistent implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            ItemStack[] inventory = player.getInventory().getContents();
+
+            boolean error = false;
+            outerloop: // Label to break out to
+            for (int i = 0; i < inventory.length; i++) {
+                ItemStack is = inventory[i];
+                if (is != null) { // Skip if null to avoid NullPointerException
+                    if (is.getType() ==  Material.EMERALD && is.getItemMeta().getDisplayName().equals("Reinforced Emerald") && is.getItemMeta().hasLore()) {
+                        if (is.getItemMeta().getLore().get(0).equals("Persistent")) {
+
+
+                            //Grab data of OffHand item
+                            ItemStack ohItem = player.getInventory().getItemInOffHand();
+                            ItemStack bigStack = ohItem.clone();
+                            ItemStack bigEmerald = is.clone();
+                            boolean giveBig = false;
+
+                            if (ohItem.getAmount() > 1) {
+                                bigStack.setAmount((ohItem.getAmount() - 1));
+                                ohItem.setAmount(1);
+                                giveBig = true;
+                            }
+
+                            //Add Persistence to OffHand item
+                            ItemMeta ohItemMeta = ohItem.getItemMeta();
+
+                            if (ohItemMeta == null) {
+                                sender.sendMessage("Nothing in offhand to enchant");
+                                error = true;
+                                break outerloop;
+                            }
+
+                            if (ohItemMeta.hasLore()) {
+                                List<String> lore = ohItemMeta.getLore();
+                                if (lore.contains("Persistent")) {
+                                    sender.sendMessage("This item is already persistent.");
+                                    error = true;
+                                    break outerloop;
+                                }
+                                // lore.add("Persistent"); //Not used because it's bypassed with the above error
+                                // ohItemMeta.setLore(lore);
+                            } else {
+                                ArrayList<String> lore = new ArrayList<>();
+                                lore.add("Persistent");
+                                ohItemMeta.setLore(lore);
+                            }
+
+                            ohItem.setItemMeta(ohItemMeta);
+
+                            player.getInventory().setItemInOffHand(ohItem);
+                            sender.sendMessage("Enchanted!");
+                            error = true; //Using error as a flag to stop the Emerald not found message
+                            if (giveBig) {
+                                player.getWorld().dropItemNaturally(player.getLocation(), bigStack);
+                            }
+                            //Take away the Reinforced Emerald
+                            player.getInventory().setItem(i,  new ItemStack(Material.AIR));
+                            boolean giveBigEmerald = false;
+
+                            if (is.getAmount() > 1) {
+                                bigEmerald.setAmount((is.getAmount() - 1));
+                                giveBigEmerald = true;
+                            }
+
+                            if (giveBigEmerald) {
+                                player.getWorld().dropItemNaturally(player.getLocation(), bigEmerald);
+                            }
+
+                            break outerloop;
+
+
+                        }
+
+                    }
+                }
+            }
+
+            if (!error) {
+                sender.sendMessage("No Reinforced Emerald found, could not enchant.");
+            }
+            return true;
+
+        } else {
+            sender.sendMessage("Only players should use this command");
+            return false;
+        }
+
+
+
+    }
+
+}
---
+++ src/main/java/com/floogoobooq/blackomega/paperpersistence/PaperPersistence.java
@@ -0,0 +1,283 @@
+package com.floogoobooq.blackomega.paperpersistence;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+//import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+//import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+// import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+//import de.tr7zw.nbtapi.NBTCompound;
+//import de.tr7zw.nbtinjector.NBTInjector;
+
+public class PaperPersistence extends JavaPlugin implements Listener {
+
+
+    public void onEnable() {
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+        //registerPersistence();
+        //NamespacedKey id = new NamespacedKey(this, "PERSISTENCE");
+        //Persistence persistence = new Persistence(id);
+		/*
+		// Create the Oops Potato
+		ItemStack oopsPotato = new ItemStack(Material.POISONOUS_POTATO);
+		ItemMeta potatoMeta = oopsPotato.getItemMeta();
+		potatoMeta.setDisplayName("oops");
+		oopsPotato.setItemMeta(potatoMeta);
+
+		NamespacedKey recipeKey = new NamespacedKey(this, "persistenceRecipe");
+		ShapedRecipe persistenceRecipe = new ShapedRecipe(recipeKey, oopsPotato);
+
+		persistenceRecipe.shape("###", "#o#", "###");
+		persistenceRecipe.setIngredient('#', Material.EMERALD);
+		persistenceRecipe.setIngredient('o',  )
+		*/
+
+        // Check if KeepInventory is enabled, recommend disabling it
+        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            public void run() {
+                List<String> keepInventoryWorlds = new ArrayList<String>();
+                for (World world : getServer().getWorlds()) {
+                    Boolean keepInventoryEnabled = world.getGameRuleValue(GameRule.KEEP_INVENTORY);
+                    if (keepInventoryEnabled != null && keepInventoryEnabled) {
+                        keepInventoryWorlds.add(world.getName());
+                    }
+                }
+
+                if (keepInventoryWorlds.size() == 0) {
+                    getLogger().log(Level.INFO, "KeepInventory is disabled, Persistence will work correctly.");
+                } else if (keepInventoryWorlds.size() == 1) {
+                    Bukkit.broadcastMessage("KeepInventory is enabled in world \"" + keepInventoryWorlds.get(0) + "\". For the Persistence plugin to work properly, this should be disabled.");
+                } else {
+                    Bukkit.broadcastMessage("KeepInventory is enabled in multiple worlds. For the Persistence plugin to work properly, this should be disabled. See the server console for a list of worlds with KeepInventory enabled.");
+                    StringBuilder listString = new StringBuilder();
+                    for (String world : keepInventoryWorlds) {
+                        listString.append(world);
+                        listString.append("; ");
+                    }
+                    getLogger().log(Level.INFO, listString.toString());
+                }
+            }
+        });
+
+        // Create the Reinforced Emerald
+        ItemStack reinforcedEmerald = new ItemStack(Material.EMERALD);
+        ItemMeta reMeta = reinforcedEmerald.getItemMeta();
+        reMeta.setDisplayName("Reinforced Emerald");
+        ArrayList<String> lore = new ArrayList<>();
+        lore.add("Persistent");
+        reMeta.setLore(lore);
+        reinforcedEmerald.setItemMeta(reMeta);
+
+        // Set up shapeless crafting recipe for Reinforced Emerald
+        NamespacedKey recipeKey = new NamespacedKey(this, "reRecipe");
+        ShapelessRecipe reRecipe = new ShapelessRecipe(recipeKey, reinforcedEmerald);
+        reRecipe.addIngredient(1, Material.EMERALD);
+        reRecipe.addIngredient(8, Material.IRON_INGOT);
+
+        getServer().addRecipe(reRecipe);
+
+        // Register the command
+        this.getCommand("makepersistent").setExecutor(new CommandMakePersistent());
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerDeath(final PlayerDeathEvent event) {
+        final Player player = (Player) event.getEntity();
+
+        final ItemStack[] inventory = player.getInventory().getContents();
+
+        for(int i = 0; i < inventory.length; i++) { //Loop through inventory
+
+            ItemStack is = inventory[i];
+            if(is != null) {
+
+                if(is.getItemMeta() instanceof BlockStateMeta) { //Special case for shulker boxes
+                    BlockStateMeta im = (BlockStateMeta)is.getItemMeta();
+                    if(im.getBlockState() instanceof ShulkerBox) {
+                        ShulkerBox shulker = (ShulkerBox) im.getBlockState();
+
+                        for (ItemStack s : shulker.getInventory().getContents()) {
+                            if(s != null ) {
+                                if (s.getItemMeta().hasLore()) {
+                                    List<String> lore = s.getItemMeta().getLore();
+                                    if (!lore.contains("Persistent")) {
+                                        player.getWorld().dropItemNaturally(player.getLocation(), s);
+                                        s.setAmount(0);
+                                    } else {
+                                        event.getDrops().remove(s);
+                                    }
+                                } else {
+                                    player.getWorld().dropItemNaturally(player.getLocation(), s);
+                                    s.setAmount(0);
+                                }
+                            }
+                        }
+
+                        im.setBlockState(shulker);
+                        is.setItemMeta(im);
+
+						/*ItemStack[] shulkerInv = shulker.getInventory().getContents();
+						for(int s = 0; s < shulkerInv.length; i++) {
+							ItemStack ss = shulkerInv[s];
+							if(ss != null) {
+								if (ss.getItemMeta().hasLore()) {
+									List<String> lore = ss.getItemMeta().getLore();
+									if (!lore.contains("Persistent")) {
+										shulkerInv[s] = null;
+									} else {
+										event.getDrops().remove(ss);
+									}
+								} else {
+									shulkerInv[s] = null;
+								}
+							}
+						}
+						*/
+
+                    }
+                }
+
+
+                if (is.getItemMeta().hasLore()) {
+                    List<String> lore = is.getItemMeta().getLore();
+                    if (!lore.contains("Persistent")) {
+                        inventory[i] = null;
+                    } else {
+                        event.getDrops().remove(is);
+                    }
+                } else {
+                    inventory[i] = null;
+                }
+            }
+        }
+
+        Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+
+            @Override
+            public void run() {
+                player.getInventory().setContents(inventory);
+            }
+
+        });
+
+    }
+
+	/*@EventHandler
+	public void onBlockPlace(final BlockPlaceEvent event) {
+
+		//Player player = event.getPlayer();
+		//EquipmentSlot playerHand = event.getHand();  //Will use this if offhand causes issues
+
+		ItemStack placedItem = event.getItemInHand();
+		if(placedItem.getItemMeta().hasLore()) {
+			List<String> itemLore = placedItem.getItemMeta().getLore();
+			if(itemLore.contains("Persistent")) {
+				Block placedBlock = event.getBlockPlaced();
+				NBTCompound comp = NBTInjector.getNbtData(placedBlock.getState());
+				comp.setString("Persistent", "yes");
+			}
+		}
+
+
+	}*/
+
+	/*@EventHandler
+	public void onBlockBreak(final BlockBreakEvent event) {
+		Block blockBroken = event.getBlock();
+		NBTCompound comp = NBTInjector.getNbtData(blockBroken.getState());
+		if(comp.hasKey("Persistent") != null) {
+			if(comp.hasKey("Persistent")) {
+				String persistent = comp.getString("Persistent");
+				if(persistent.equals("yes")) {
+					Collection<ItemStack> isCollection = blockBroken.getDrops();
+					Collection<ItemStack> newCollection = new ArrayList<ItemStack>();
+					for (Iterator<ItemStack> iterator = isCollection.iterator(); iterator.hasNext();) {
+						ItemStack item = iterator.next();
+						if(item.getItemMeta().hasLore()) {
+							List<String> lore = item.getItemMeta().getLore();
+							if(!lore.contains("Persistent")) {
+								lore.add("Persistent");
+								item.getItemMeta().setLore(lore);
+							}
+
+						}
+						newCollection.add(item);
+					}
+
+					blockBroken.setType(Material.AIR);
+					for (Iterator<ItemStack> newIterator = newCollection.iterator(); newIterator.hasNext();) {
+						blockBroken.getWorld().dropItemNaturally(blockBroken.getLocation(), newIterator.next());
+					}
+
+
+				}
+			}
+		}
+
+
+
+
+	}*/
+
+    @EventHandler
+    public void onBlockBreak(final BlockBreakEvent event) {
+        Block blockBroken = event.getBlock();
+        if(Tag.SHULKER_BOXES.isTagged(blockBroken.getType())) {
+            Collection<ItemStack> isCollection = blockBroken.getDrops();
+            Collection<ItemStack> newCollection = new ArrayList<ItemStack>();
+            for(Iterator<ItemStack> iterator = isCollection.iterator(); iterator.hasNext();) {
+                ItemStack item = iterator.next();
+                ItemMeta meta = item.getItemMeta();
+                if(meta.hasLore()) {
+                    List<String> lore = item.getItemMeta().getLore();
+                    if(!lore.contains("Persistent")) {
+                        lore.add("Persistent");
+                        meta.setLore(lore);
+                    }
+
+                } else {
+                    ArrayList<String> lore = new ArrayList<>();
+                    lore.add("Persistent");
+                    meta.setLore(lore);
+                }
+
+                item.setItemMeta(meta);
+                newCollection.add(item);
+            }
+
+            blockBroken.setType(Material.AIR);
+            for (Iterator<ItemStack> newIterator = newCollection.iterator(); newIterator.hasNext();) {
+                blockBroken.getWorld().dropItemNaturally(blockBroken.getLocation(), newIterator.next());
+            }
+        }
+    }
+
+
+}
