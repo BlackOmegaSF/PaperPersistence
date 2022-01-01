14bb69a2431992d32acd6decd5cd546bd61e41ae
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-01 14:10:43 -0800

    Annotate parameters properly

--- src/main/java/com/floogoobooq/blackomega/paperpersistence/CommandMakePersistent.java
+++ src/main/java/com/floogoobooq/blackomega/paperpersistence/CommandMakePersistent.java
@@ -8,11 +8,12 @@ import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 public class CommandMakePersistent implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             ItemStack[] inventory = player.getInventory().getContents();
