8d7cb417595f90d4332bc2fae45a792ecbde5155
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-01 20:21:11 -0800

    Optimize imports

--- src/main/java/com/floogoobooq/blackomega/paperpersistence/TabCompletion.java
+++ src/main/java/com/floogoobooq/blackomega/paperpersistence/TabCompletion.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class TabCompletion implements TabCompleter {
